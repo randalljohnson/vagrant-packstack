036137318a1c6f1741db23d2c3147e6f05721c7b
Randy Johnson <rajohnso@redhat.com>
2018-10-31 13:32:11 -0400

    ignore erros on network restarts

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 vagrant destroy -f
-set -x
 vagrant up
-set +x
-vagrant ssh -c 'sudo /vagrant/configure_network.sh'
+vagrant ssh -c 'sudo /vagrant/configure_network.sh' || :
 sleep 2
-vagrant ssh -c 'sudo systemctl restart neutron-openvswitch-agent neutron-server'
+vagrant ssh -c 'sudo systemctl restart neutron-openvswitch-agent neutron-server' || :
 vagrant ssh -c 'sudo /vagrant/deploy_demo.sh'
